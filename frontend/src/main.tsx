b2423a71785a0fc358d3e33211e4e0a18db0b175
Lalit Verma <lv2022244@gmial.com>
2026-05-18 20:41:30 +0530

    added Vercel Analytics & Speed Insights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
@@ -7,5 +9,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
